804cd5f9bdd3c439df57277f97b86b12652e7c69
rart <royart@me.com>
2020-04-22 16:47:31 +0200

    Add missing file

---
+++ sources/react-app/src/shared/queries.graphql.js
@@ -0,0 +1,237 @@
+/*
+ * Copyright (C) 2007-2020 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// language=GraphQL
+export default `
+
+  fragment byUrlQueryHomepage on page_entry  {
+    pageTitle_s
+    pageDescription_s
+    bios_o {
+      item {
+        key
+        component {
+          ...byUrlQueryBioFragment
+        }
+      }
+    }
+    slider_o {
+      item {
+        key
+        component {
+          ...byUrlQueryContentItemFields
+          ...on component_slider {
+            ...byUrlQuerySlider
+          }
+        }
+      }
+    }
+  }
+  
+  fragment byUrlQueryAboutPage on page_about {
+    pageTitle_s
+    pageDescription_s
+    headline_s
+    bios_o {
+      item {
+        key
+        component {
+          ...byUrlQueryBioFragment
+        }
+      }
+    }
+    content_o {
+      item {
+        key
+        component {
+          ...on component_rich_text {
+            ...byUrlQueryRichText
+          }
+          ...on component_image {
+            ...byUrlQueryImage
+          }
+          ...on component_responsive_columns {
+            ...byUrlQueryResponsiveColumns
+          }
+        }
+      }
+    }
+  }
+
+  fragment byUrlQueryContactPage on page_contact {
+    pageTitle_s
+    pageDescription_s
+
+  }
+
+  fragment byUrlQueryCategoryPage on page_category {
+    pageTitle_s
+    pageDescription_s
+
+  }
+
+  fragment byUrlQueryPostPage on page_post {
+    ...byUrlQueryContentItemFields
+    slug: localId(transform: "storeUrlToRenderUrl")
+    pageTitle_s
+    pageDescription_s
+    blurb_t
+    headline_s
+    mainImage_s
+    content_o {
+      item {
+        key
+        component {
+          ...on component_rich_text {
+            ...byUrlQueryRichText
+          }
+          ...on component_image {
+            ...byUrlQueryImage
+          }
+          ...on component_responsive_columns {
+            ...byUrlQueryResponsiveColumns
+          }
+        }
+      }
+    }
+    authorBio_o {
+      item {
+        key
+        component {
+          ...byUrlQueryBioFragment
+        }
+      }
+    }
+  }
+
+  fragment byUrlQueryContentItemFields on ContentItem {
+    guid: objectId
+    path: localId
+    contentTypeId: content__type
+    dateCreated: createdDate_dt
+    dateModified: lastModifiedDate_dt
+    label: internal__name
+  }
+
+  fragment byUrlQueryBioFragment on component_bio {
+    guid: objectId
+    contentTypeId: content__type
+    label: internal__name
+    path: localId
+    bio_t
+    name_s
+    profilePic_s
+    linkButtonText_s
+    linkButtonUrl_s
+    showLinkButton_b
+  }
+
+  fragment byUrlQueryRichText on component_rich_text {
+    ...byUrlQueryContentItemFields
+    content_html_raw
+  }
+
+  fragment byUrlQueryImage on component_image {
+    ...byUrlQueryContentItemFields
+    image_s
+    alternativeText_s
+  }
+
+  fragment byUrlQueryResponsiveColumns on component_responsive_columns {
+    ...byUrlQueryContentItemFields
+    columns_o {
+      item {
+        columnSize_s
+        content_o {
+          item {
+            key
+            component {
+              ...on component_rich_text {
+                ...byUrlQueryRichText
+              }
+              ...on component_image {
+                ...byUrlQueryImage
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  fragment byUrlQuerySlider on component_slider {
+    ...byUrlQueryContentItemFields
+    posts_o {
+      item {
+        key
+        component {
+          ...byUrlQueryContentItemFields
+          slug: localId(transform: "storeUrlToRenderUrl")
+          pageTitle_s
+          pageDescription_s
+          blurb_t
+          headline_s
+          mainImage_s
+        }
+      }
+    }
+  }
+
+  query byUrlQuery(
+    $url: String, $skipContentType: Boolean = true
+    $includePosts: Boolean = true
+    $postsLimit: Int = 8
+    $postsOffset: Int = 0
+  ) {
+    content: contentItems {
+      total
+      items {
+        ...byUrlQueryContentItemFields
+        url: localId(
+          transform: "storeUrlToRenderUrl",
+          filter:{ regex: $url }
+        )
+        content__type(
+          filter:{
+            regex: ".*(bio|post|entry|category|contact|about).*"
+          }
+        ) @skip (if: $skipContentType)
+        ...on page_entry {
+          ...byUrlQueryHomepage
+        }
+        ...on page_about {
+          ...byUrlQueryAboutPage
+        }
+        ...on page_contact {
+          ...byUrlQueryContactPage
+        }
+        ...on page_category {
+          ...byUrlQueryCategoryPage
+        }
+        ...on page_post {
+          ...byUrlQueryPostPage
+        }
+      }
+    }
+    posts: page_post(limit: $postsLimit, offset: $postsOffset) @include(if: $includePosts) {
+      total
+      items {
+        ...byUrlQueryPostPage
+      }
+    }
+  }
+  
+`;
