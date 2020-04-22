3e6cf0c5b7417e8bccfd8869ff7754310447248a
rart <royart@me.com>
2020-04-22 14:05:59 +0200

    Updates
    - Add crossorigin to manifest link
    - App build

---
+++ static-assets/app/precache-manifest.24559c5a3fa760b6f753e94a207c8196.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a92cdb19138c837a7321631bcc3028",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "34d4bf83c42c78efa982",
+    "url": "/static-assets/app/static/js/10.722eb1b1.chunk.js"
+  },
+  {
+    "revision": "fd849c34a65f52532c04",
+    "url": "/static-assets/app/static/js/11.ee1ed0d3.chunk.js"
+  },
+  {
+    "revision": "d6209f593c8fb688a976",
+    "url": "/static-assets/app/static/js/12.c1f5a2ad.chunk.js"
+  },
+  {
+    "revision": "8c597772a4ba42fa25f7",
+    "url": "/static-assets/app/static/js/2.ee59d50f.chunk.js"
+  },
+  {
+    "revision": "f8f2af7ae91e9be4fb78d6cf665b36ab",
+    "url": "/static-assets/app/static/js/2.ee59d50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f624ac9125b7749712",
+    "url": "/static-assets/app/static/js/3.c82795a4.chunk.js"
+  },
+  {
+    "revision": "5c10dff3d47613967cba",
+    "url": "/static-assets/app/static/js/4.104fc3c8.chunk.js"
+  },
+  {
+    "revision": "06100211551ceb034aa8",
+    "url": "/static-assets/app/static/js/5.e2084797.chunk.js"
+  },
+  {
+    "revision": "a2520ce790229073a409",
+    "url": "/static-assets/app/static/js/6.6e2e82be.chunk.js"
+  },
+  {
+    "revision": "819cecef237595e9d62d",
+    "url": "/static-assets/app/static/js/7.1d56d670.chunk.js"
+  },
+  {
+    "revision": "a3d433c66d27fb76ea50",
+    "url": "/static-assets/app/static/js/8.181c197a.chunk.js"
+  },
+  {
+    "revision": "176d625d59db815288b7",
+    "url": "/static-assets/app/static/js/9.d8f41bac.chunk.js"
+  },
+  {
+    "revision": "bce5b9be702fe24ccd5c",
+    "url": "/static-assets/app/static/js/main.b794f594.chunk.js"
+  },
+  {
+    "revision": "c67ed315d22acd0afa3a",
+    "url": "/static-assets/app/static/js/runtime-main.414a3f7b.js"
+  }
+]);
--- static-assets/app/service-worker.js
+++ static-assets/app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static-assets/app/precache-manifest.22591c173d2157df93e88a1e0e9d5784.js"
+  "/static-assets/app/precache-manifest.24559c5a3fa760b6f753e94a207c8196.js"
 );
 
 self.addEventListener('message', (event) => {
