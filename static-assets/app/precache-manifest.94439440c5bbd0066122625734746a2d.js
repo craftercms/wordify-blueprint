44d0709eff4bec8f45fc1555bb70d4149bd398e1
jvega190 <jose.vega@craftersoftware.com>
2020-06-02 16:51:44 -0600

    Update build

---
+++ static-assets/app/precache-manifest.94439440c5bbd0066122625734746a2d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb179b76d335e96ac3803da96931d30",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "a17fbccf956e680fee80",
+    "url": "/static-assets/app/static/js/10.78e49d2a.chunk.js"
+  },
+  {
+    "revision": "e04ad3f4e20a8ab80b8b",
+    "url": "/static-assets/app/static/js/11.cc5d15f9.chunk.js"
+  },
+  {
+    "revision": "2327aa89d60085823290",
+    "url": "/static-assets/app/static/js/12.14bf107c.chunk.js"
+  },
+  {
+    "revision": "768e2df73460fbc58215",
+    "url": "/static-assets/app/static/js/13.e49fdd75.chunk.js"
+  },
+  {
+    "revision": "57aa2d37905149ecd824",
+    "url": "/static-assets/app/static/js/2.e15c5b6c.chunk.js"
+  },
+  {
+    "revision": "f8f2af7ae91e9be4fb78d6cf665b36ab",
+    "url": "/static-assets/app/static/js/2.e15c5b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9996a3813b77b4973923",
+    "url": "/static-assets/app/static/js/3.1e0aa568.chunk.js"
+  },
+  {
+    "revision": "c87c859553b70a0d27ac",
+    "url": "/static-assets/app/static/js/4.033799e3.chunk.js"
+  },
+  {
+    "revision": "4e266f5ded19fa25e574",
+    "url": "/static-assets/app/static/js/5.c68cde6b.chunk.js"
+  },
+  {
+    "revision": "66ac1bee614cda236e9b",
+    "url": "/static-assets/app/static/js/6.a430ebeb.chunk.js"
+  },
+  {
+    "revision": "fa02e6d26af6a4a1089f",
+    "url": "/static-assets/app/static/js/7.cb2f7a1b.chunk.js"
+  },
+  {
+    "revision": "ed7365de8c0e12f9145f",
+    "url": "/static-assets/app/static/js/8.1d978b56.chunk.js"
+  },
+  {
+    "revision": "cc0be2cc615d3b53ee49",
+    "url": "/static-assets/app/static/js/9.cd747b60.chunk.js"
+  },
+  {
+    "revision": "515d62d53477d94d365f",
+    "url": "/static-assets/app/static/js/main.057a31ec.chunk.js"
+  },
+  {
+    "revision": "a167c3ed474b79e82eb3",
+    "url": "/static-assets/app/static/js/runtime-main.ebfbc02b.js"
+  }
+]);
--- static-assets/app/service-worker.js
+++ static-assets/app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static-assets/app/precache-manifest.c60b0170b4070e09f6d1066e3cbe9d79.js"
+  "/static-assets/app/precache-manifest.94439440c5bbd0066122625734746a2d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/static-assets/app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
