1cd31fa5fbc48d2c4d840fd767869318cea9b635
jvega190 <jose.vega@craftersoftware.com>
2020-10-09 15:54:59 -0600

    Fix - navigate to a post from category view flows through categories view

--- sources/react-app/src/pages/Category.js
+++ sources/react-app/src/pages/Category.js
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, { useState, Suspense } from 'react';
+import React, { useState, Suspense, useEffect } from 'react';
 import BaseLayout from '../shared/BaseLayout';
 import RecentPostsAside from '../shared/RecentPostsAside';
 import PostCard, { LANDSCAPE } from '../shared/PostCard';
@@ -120,8 +120,14 @@ export default function (props) {
   } = props;
 
   const isTag = match.path === '/tag/:id?';
-  const categoryId = match.params.id;
+  const [categoryId, setCategoryId] = useState();
   let resource = useTaxonomiesResource();
+
+  useEffect(() => {
+    if(match.path === '/category/:id?' || match.path === '/tag/:id?') {
+      setCategoryId(match.params.id);
+    }
+  }, [match.params]);
 
   return (
     <BaseLayout siteTitle={siteTitle} socialLinks={socialLinks}>
