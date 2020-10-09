b88f82359ea82fd2454dea66df6ec201a543e921
jvega190 <jose.vega@craftersoftware.com>
2020-10-09 15:56:55 -0600

    Update build

---
+++ static-assets/app/precache-manifest.b55056eb900e5bc9299c1a0686923f6e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0257dd9b175118d6d44320d18f15c4c7",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "0a70abdd4f1271c101f2",
+    "url": "/static-assets/app/static/js/0.f11bce59.chunk.js"
+  },
+  {
+    "revision": "e2d804cdcfb9d3c5276d",
+    "url": "/static-assets/app/static/js/10.160a81be.chunk.js"
+  },
+  {
+    "revision": "b62148b84b855de8c758",
+    "url": "/static-assets/app/static/js/11.9af32e1c.chunk.js"
+  },
+  {
+    "revision": "20f886d21d2c5098978c",
+    "url": "/static-assets/app/static/js/12.98ae15ee.chunk.js"
+  },
+  {
+    "revision": "6236a90663083394fc24",
+    "url": "/static-assets/app/static/js/13.619360e9.chunk.js"
+  },
+  {
+    "revision": "ddb3ca71132f5f1b0d2d",
+    "url": "/static-assets/app/static/js/14.7aeb6e49.chunk.js"
+  },
+  {
+    "revision": "cbc8fdae99e84fe49181",
+    "url": "/static-assets/app/static/js/3.99dd7bfc.chunk.js"
+  },
+  {
+    "revision": "759e151e4ec49cb6a76d22a5b851998a",
+    "url": "/static-assets/app/static/js/3.99dd7bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73973c8c90459652509a",
+    "url": "/static-assets/app/static/js/4.4abdfb91.chunk.js"
+  },
+  {
+    "revision": "b240c989a04e77217e15",
+    "url": "/static-assets/app/static/js/5.151edcb3.chunk.js"
+  },
+  {
+    "revision": "039f69e61bcef76609ac",
+    "url": "/static-assets/app/static/js/6.7470afe1.chunk.js"
+  },
+  {
+    "revision": "8b0ec8e1694322092047",
+    "url": "/static-assets/app/static/js/7.6877272d.chunk.js"
+  },
+  {
+    "revision": "49702e77964db7679ec6",
+    "url": "/static-assets/app/static/js/8.1df00c91.chunk.js"
+  },
+  {
+    "revision": "e6c6b11a7dc43e15d818",
+    "url": "/static-assets/app/static/js/9.72ba1f16.chunk.js"
+  },
+  {
+    "revision": "c198b5f516aa48db4a71",
+    "url": "/static-assets/app/static/js/main.38d95869.chunk.js"
+  },
+  {
+    "revision": "b37aed5fe2ef1e9e07a1",
+    "url": "/static-assets/app/static/js/runtime-main.940a128d.js"
+  }
+]);
--- static-assets/app/static/js/12.c06b28ab.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpwordify-craftercms-react-app"]=this["webpackJsonpwordify-craftercms-react-app"]||[]).push([[12],{211:function(e,a,t){"use strict";t.r(a);var r=t(0),c=t.n(r);a.default=function(e){var a=e.ice,t=e.model,r=t.image_s,s=t.alternativeText_s;return c.a.createElement(c.a.Fragment,null,c.a.createElement("img",Object.assign({},a,{src:r,alt:s||"",className:"img-fluid"})))}}}]);
-//# sourceMappingURL=12.c06b28ab.chunk.js.map
