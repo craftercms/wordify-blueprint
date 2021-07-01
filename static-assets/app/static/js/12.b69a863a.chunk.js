63f4296d24258e8ca2f885bc8396efe3b31f7f8c
jvega190 <josevega19@gmail.com>
2021-07-01 11:15:24 -0600

    Build update

---
+++ static-assets/app/static/js/12.b69a863a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@craftercms/wordify-react"]=this["webpackJsonp@craftercms/wordify-react"]||[]).push([[12],{149:function(e,t,r){"use strict";r.r(t);var a=r(0),c=r.n(a),n=r(14);t.default=function(e){var t=e.model;return c.a.createElement(n.b,{model:t},c.a.createElement(n.d,{component:"img",model:t,fieldId:"image_s,alternativeText_s",renderTarget:"src,alt",className:"img-fluid"}))}}}]);
+//# sourceMappingURL=12.b69a863a.chunk.js.map
